36d3c3c3617f42907bd7b7b43c5edac5d37fe8f7
iradukun <mustaphairadukunda@gmail.com>
2022-10-07 16:23:22 +0200

    removed with credentials on firstlogin

--- client/src/constant/constant.js
+++ client/src/constant/constant.js
@@ -1,5 +1,5 @@
 import Axios from "axios"
-
+export const proxyurl = "https://cors-anywhere.herokuapp.com/";
 export const BASEURL = "https://spare-part.herokuapp.com"
 
 export const _axios = Axios.create({withCredentials: true})
