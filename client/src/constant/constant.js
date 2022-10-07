e43d1da5a32fb7e4e2e70a7cb846872a2bb7898c
iradukun <mustaphairadukunda@gmail.com>
2022-10-07 21:51:14 +0200

    changed to proodu

--- client/src/constant/constant.js
+++ client/src/constant/constant.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 export const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// export const BASEURL = "https://spare-part.herokuapp.com"
-export const BASEURL = "http://localhost:5000"
+export const BASEURL = "https://spare-part.herokuapp.com"
+// export const BASEURL = "http://localhost:5000"
 
 export const _axios = Axios.create({withCredentials: true})
