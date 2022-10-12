21c8dc74e79caecd37f56e72d8fc2ae18f63d5e6
iradukun <mustaphairadukunda@gmail.com>
2022-10-12 20:43:09 +0200

    resolving

---
+++ client/build/precache-manifest.34a23eef7d2b648c8879beb1608c4633.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ae5eda4c648e5e2936ec46ea5ddd7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977583a4c628ed102b86",
+    "url": "/static/css/main.1d29729c.chunk.css"
+  },
+  {
+    "revision": "3b4035d1df947c6bcc0f",
+    "url": "/static/js/2.a83f1548.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a83f1548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977583a4c628ed102b86",
+    "url": "/static/js/main.4ece6a8c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
--- client/src/api/ProductsAPI.js
+++ client/src/api/ProductsAPI.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 import {_axios as axios} from '../constant/constant'
-import {BASEURL} from '../index'
+import {BASEURL} from '../constant/constant'
 
 
 function ProductsAPI() {
