8a48f3fca329cc9515bc8b4f2daaec0b3ea4127e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-24 23:08:02 +0200

    solved a dist issue and added images banner downloaded

--- client/src/components/carousels.tsx
+++ client/src/components/carousels.tsx
@@ -8,9 +8,9 @@ const Carousels = () => {
     setIndex(selectedIndex);
   };
   const images = [
-    "https://germaine.ng/wp-content/uploads/2019/11/Vero.banner2.jpeg",
-    "http://dhanyaautospareparts.dhanyagroup.com/data1/images/banner4.jpg",
-    "http://dhanyaautospareparts.dhanyagroup.com/data1/images/banner3.jpg",
+    "/images/car_banner1.jpeg",
+    "/images/car_banner2.jpg",
+    "/images/car_banner3.jpeg",
   ];
   return (
     <Carousel
@@ -21,7 +21,7 @@ const Carousels = () => {
       {images.map((image, index) => (
         <Carousel.Item key={index} className="carsouel__item">
           <img
-            // className="d-block w-full"
+            className="d-block w-full"
             src={image}
             alt="First slide"
           />
